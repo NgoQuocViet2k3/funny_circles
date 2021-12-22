5a250ec0c9b271826cc78fcdc7ffd350f9c73c63
ruukichi <ngovietlc93@gmail.com>
2021-12-22 14:54:32 +0700

    funny circles

---
+++ funny_circle.js
@@ -0,0 +1,30 @@
+class Circle {
+    x;
+    y;
+    radius;
+    color;
+
+    constructor(x, y, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    getX() {
+        return this.x;
+    }
+
+    getY() {
+        return this.y;
+    }
+
+    getRadius() {
+        return this.radius;
+    }
+
+    getColor() {
+        return this.color;
+    }
+
+}
